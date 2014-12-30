3222078145764749517fa7208ec9fb448ec4a153
Tavern <windack@qq.com>
2014-12-31 00:47:09 +0800

    some bugs

--- app/api/tag_api.rb
+++ app/api/tag_api.rb
@@ -1,8 +1,4 @@
 class Etrain::TagApi < Grape::API
-  before_validation do
-    error!({message: '请先登录后再试！'}.as_json, 403) unless @current_user
-  end
-
   resources :tags do
     get do
       Tag.all
---
+++ db/seeds.rb
@@ -0,0 +1,1 @@
+User.reg 'tavern', 'Tavern1001', 1
