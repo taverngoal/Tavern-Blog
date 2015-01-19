504d1dd7e5bbf8d4c95d0ebe498c75ea39a92304
Tavern <windack@qq.com>
2015-01-19 17:37:21 +0800

    修改目录问题

--- server.rb
+++ server.rb
@@ -5,7 +5,7 @@ class Application < Goliath::API
   use Goliath::Rack::Render
   use ActiveRecord::ConnectionAdapters::ConnectionManagement
   # use Goliath::Rack::Render, 'json'
-  use Rack::TryStatic, :root => 'web', :urls => %w[/ /assets /views], :try => %w(.html .js .css index.html)
+  use Rack::TryStatic, :root => "#{__dir__}/web", :urls => %w[/ /assets /views], :try => %w(.html .js .css index.html)
   def response(env)
     ::Etrain.call(env)
   end
